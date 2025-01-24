c2db402f70743b5de6fcfb866da7cb98afc23daf
Guyutongxue <guyutongxue@163.com>
2025-01-24 17:14:52 +0800

    update

--- api/v2/data/[id].js
+++ api/v2/data/[id].js
@@ -13,11 +13,17 @@ import { all, keywords } from "../data.js";
  * @param {VercelResponse} res
  * @returns
  */
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { id } = req.query;
   if (Array.isArray(id)) {
     res.status(400)
       .send("Bad request (multiple id)");
+    return;
+  }
+  const { Accept } = req.headers;
+  if (Accept.includes("image/webp")) {
+    const response = await fetch(`https://gi-tcg-card-data-img.vercel.app/${id}.webp`);
+    res.status(200).send(response);
     return;
   }
   let found;
