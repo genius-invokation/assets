5c2276c8d45d7a3eef73c06b7e4cb4516a6cb915
Guyutongxue <guyutongxue@163.com>
2025-04-01 23:40:06 +0800

    add category

--- src/node/generate_data.ts
+++ src/node/generate_data.ts
@@ -10,21 +10,22 @@ import { outputDir } from "./config";
 
 const GITHUB_CONTENT_BASE = `https://raw.githubusercontent.com/genius-invokation/genius-invokation/refs/heads/main/packages/static-data/src/data`;
 
-const FILENAMES = [
-  "action_cards.json",
-  "characters.json",
-  "entities.json",
-  "keywords.json",
-];
+const FILENAMES = ["action_cards", "characters", "entities", "keywords"];
 
 const downloaded: Record<string, any[]> = {};
 
 for (const filename of FILENAMES) {
-  const data = await fetch(`${GITHUB_CONTENT_BASE}/${filename}`).then((r) =>
-    r.text(),
+  const data = await fetch(`${GITHUB_CONTENT_BASE}/${filename}.json`).then(
+    (r) => r.text(),
   );
-  downloaded[filename] = JSON.parse(data);
-  await Bun.write(`${outputDir}/${filename}`, data);
+  downloaded[filename] = JSON.parse(data).map((src) => ({
+    ...src,
+    category: filename,
+  }));
+  await Bun.write(
+    `${outputDir}/${filename}.json`,
+    JSON.stringify(downloaded[filename]),
+  );
 }
 
 export const actionCards: ActionCardRawData[] = downloaded["action_cards.json"];
